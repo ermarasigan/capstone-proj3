f48e1ad55caf4efbe3129b3a55e296792ad89653
ermarasigan <ermarasigan@gmail.com>
2017-08-05 07:09:10 +0800

    tag controller

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Tag;
+use App\Doc;
+use App\User;
+use Auth;
 
 class TagController extends Controller
 {
@@ -17,7 +20,17 @@ class TagController extends Controller
     	$tags = Tag::get()->where('id','=',$id);
     	$alltags = Tag::all();
 
+         // Get doc details
+        $alldocs = Doc::all();
+
+        if(Auth::user()) {
+            $logged = Auth::user()->id;
+            $userdocs = User::find($logged)->doc()->pluck('doc_id')->toArray();
+        } else {
+            $userdocs = [];
+        }
+
         // Show blog view
-   	 	return view('blogs', compact('title','blogs','tags','alltags'));
+   	 	return view('blogs', compact('title','blogs','tags','alltags','alldocs','userdocs'));
     }
 }
